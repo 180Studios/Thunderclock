7596313ddef8f5b6cc9b3a53641d9151fe37930b
Kevin John <notkevinjohn@gmail.com>
2016-10-26 17:21:05 +0000

    wrote static functions to control each thunder cell

--- lightning.py
+++ lightning.py
@@ -13,7 +13,7 @@ GPIO.setup(17, GPIO.OUT)
 GPIO.setup(27, GPIO.OUT)
 
 #flash single cell
-def flashCell( cellNum ):
+def flashCell(cellNum):
 	flashPeriod = random.uniform(0.05, 0.1)
 	holdPeriod = random.uniform(0.1, 0.5)
 	flashRepeat = random.randint(1,4)
@@ -25,17 +25,33 @@ def flashCell( cellNum ):
 	GPIO.output(cellNum, GPIO.HIGH)
 	time.sleep(holdPeriod)
 	GPIO.output(cellNum, GPIO.LOW)
-	
+
+#specific cell functions
+def flashCell1():
+	flashCell(2)
+def flashCell2():	
+	flashCell(3)
+def flashCell3():
+	flashCell(4)
+def flashCell4():
+	flashCell(17)
+def flashCell5()
+	flashCell(27)
 
 
-length=random.randint(1,5)
-for i in range(0,length):
-	p1 = Process(target = flashCell(2))
-	p2 = Process(target = flashCell(3))
-	p3 = Process(target = flashCell(4))
-	p4 = Process(target = flashCell(17))
-	p5 = Process(target = flashCell(27))
+#main loop
+#length=random.randint(1,5)
+for i in range(0,1):
+	p1 = Process(target = flashCell, args = (2))
+	p2 = Process(target = flashCell, args = (3))
+	p3 = Process(target = flashCell, args = (4))
+	p4 = Process(target = flashCell, args = (17))
+	p5 = Process(target = flashCell, args = (27))
 	cell_array = [p1,p2,p3,p4,p5]
+	
 	while len(cell_array) > 0:
-		cell_array[0].start()
-		cell_array.remove(cell_array[0])
+		randindex = random.randint(0, len(cell_array) - 1)
+		print(randindex)
+		process = cell_array[randindex]
+		process.start()
+		cell_array.remove(process)
