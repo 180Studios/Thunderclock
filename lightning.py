0775b94e903084f354485b7eacef0c48bc9b8ea5
Kevin John <notkevinjohn@gmail.com>
2016-10-26 17:03:00 +0000

    initial commit

---
+++ lightning.py
@@ -0,0 +1,40 @@
+import RPi.GPIO as GPIO
+import time
+import random
+from multiprocessing import Process
+
+#setup
+GPIO.setmode(GPIO.BCM) 
+GPIO.setwarnings(False) 
+GPIO.setup(2, GPIO.OUT) 
+GPIO.setup(3, GPIO.OUT) 
+GPIO.setup(4, GPIO.OUT) 
+GPIO.setup(17, GPIO.OUT) 
+GPIO.setup(27, GPIO.OUT)
+
+#flash single cell
+def flashCell( cellNum ):
+	flashPeriod = random.uniform(0.05, 0.1)
+	holdPeriod = random.uniform(0.1, 0.5)
+	flashRepeat = random.randint(1,4)
+	for i in range(0, flashRepeat):
+		GPIO.output(cellNum, GPIO.HIGH)
+		time.sleep(flashPeriod)
+		GPIO.output(cellNum, GPIO.LOW)
+		time.sleep(flashPeriod)
+	GPIO.output(cellNum, GPIO.HIGH)
+	time.sleep(holdPeriod)
+	GPIO.output(cellNum, GPIO.LOW)
+	
+
+
+length=random.randint(1,5)
+print(length)
+for i in range(0,length):
+	p1 = Process(target = flashCell(2))
+	p2 = Process(target = flashCell(3))
+	p3 = Process(target = flashCell(4))
+	p4 = Process(target = flashCell(17))
+	p5 = Process(target = flashCell(27))
+	cell_array = [p1,p2,p3,p4,p5]
+	
